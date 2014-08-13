a53cb0c731181eb8b335b74a9b87bf199f7464fc
qiushui007007 <xxg6688@163.com>
2014-08-12 23:37:58 -0400

    140813: data 00 must drop

--- sharp2/sharp2.lua
+++ sharp2/sharp2.lua
@@ -1,5 +1,5 @@
 --[[
-lua的uart测试例程
+lua的sharp2测试例程
 
 ]]--
 
@@ -12,39 +12,60 @@ end
 
 -- load library
 local uart = require "luauart"
+local xucommon = require "luaxucommon"
 
 -- 底层库测试.
 --device_name = "/dev/ttyATH0"
 device_name = "/dev/ttyUSB0"
 
--- 若USB-TTL当机, 则读取的数据无效. 超过3次则重启设备.
+-- 若USB-TTL当机, 则读取的数据无效. 超过N次则重启设备.
 invalid_times = 0
-while invalid_times < 3 do
+retry_times = 10
+while invalid_times < retry_times do
 	f_vo, pc_ret = uart.sharp2_get(device_name)
-	-- 参考2.3, 1V -- 200ug(0.2mg)
-	if f_vo <= 3 then
-		k =  200;
-	end
-	f_data = f_vo * k
-	print("str = " .. pc_ret .. ", vo = " .. tostring(f_vo) .. ", PM2.5 = " .. tostring(f_data))
 
-	if f_data == 0.0 then
+	if f_vo < 0.0 then
 		invalid_times = invalid_times + 1
-		uart.delay_ms(500)
+		xucommon.delay_ms(1000)
 		print("invalid_times = " .. tostring(invalid_times))
-		if invalid_times >= 3 then
-			uart.reboot()
+
+		-- 发送Email
+		to_id = "qiushui_007@foxmail.com"
+		subject = "sharp2 read dead"
+		str_time = os.date("%Y") .. "-" .. os.date("%m") .. "-" .. os.date("%d") .. " " .. os.date("%X")
+		str_ip = xucommon.get_local_ip("auto")
+		content = str_time .. ", local_ip: " .. str_ip .. "\nwww.lewei50.com/u/g/2375" .. "\nwww.yeelink.net/devices/4420"
+		xucommon.mail_tx(to_id, subject, content)
+
+		if invalid_times >= retry_times then
+			xucommon.reboot()	--重启, 你懂的
 		end
 
 	else
 		invalid_times = 0
+		-- 参考2.3, 1V -- 200ug(0.2mg)
+		if f_vo <= 3 then
+			k =  200;
+		end
+		f_data = f_vo * k
+		str_data = "PM2.5 = " .. tostring(f_data) .. ", str = " .. pc_ret .. ", vo = " .. tostring(f_vo)
+		print(str_data)
+
+		-- 根据情况
+		to_id = "qiushui_007@foxmail.com"
+		subject = "sharp2 read OK"
+		str_time = os.date("%Y") .. "-" .. os.date("%m") .. "-" .. os.date("%d") .. " " .. os.date("%X")
+		str_ip = xucommon.get_local_ip("auto")
+		content = str_time .. ", " .. str_data
+		xucommon.mail_tx(to_id, subject, content)
+
 		break
 	end
 
 end
 
 -- 获取正确数据, 上传到云
-if f_data > 0.0 then
+if f_data > 0.0 and f_data < 560 then
 	http = require("socket.http")
   local ltn12 = require "ltn12"
 
@@ -65,6 +86,28 @@ if f_data > 0.0 then
   }
 
   print("lewei50: " .. "reqbody = " .. reqbody)
+  print('body:' .. tostring(body))
+  print('code:' .. tostring(code))
+  print('status:' .. tostring(status) .. "\n")
+
+
+  -- yeelink
+  local reqbody = "{\"value\":" .. tostring(f_data) .. "}"
+  local respbody = {}
+  local body, code, headers, status = http.request {
+      method = "POST",
+      url = "http://api.yeelink.net/v1.0/device/4420/sensor/21920/datapoints",
+      source = ltn12.source.string(reqbody),
+      headers = {
+        ["U-ApiKey"] = "729d1ba15b26b6a48f4807ef3f2f4df4",
+        ["Content-Type"] = "application/x-www-form-urlencoded",
+        ["content-length"] = string.len(reqbody),
+        ["Connection"] = "Close"
+      },
+      sink = ltn12.sink.table(respbody)
+  }
+
+  print("yeelink: " .. "reqbody = " .. reqbody)
   print('body:' .. tostring(body))
   print('code:' .. tostring(code))
   print('status:' .. tostring(status) .. "\n")
