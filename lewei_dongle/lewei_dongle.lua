f748e3f0aa4ca3deff8fdbedc3706f2efce9eb5d
qiushui007007 <xxg6688@163.com>
2014-10-30 23:50:06 -0400

    V1.1_141028 dongle

--- lewei_dongle/lewei_dongle.lua
+++ lewei_dongle/lewei_dongle.lua
@@ -1,8 +1,10 @@
 --[[
+版本Log
+V1.1: 2014-10-28, 经过深入的科学发展观而形成.
+	支持3个设备: CH340+STC+DHT22, CP2102+sharp2, DS9490+DS18B20,
+
 乐为物联的dongle的 LUA测试例程, 如下地址可以查看
 http://www.lewei50.com/u/g/2375
-
-设备总结: CH340+STC+DHT22, CP2102+sharp2, DS9490+DS18B20,
 
 准备工作
 opkg install luasocket_2.0.2-3_ar71xx.ipk
@@ -11,15 +13,16 @@ opkg install kmod-usb-serial-cp210x_3.3.8-1_ar71xx.ipk --force-depends
 
 测试操作如下:
 1. 拷贝 luauart.so, luaxucommon.so, owlua.so -> /usr/lib/lua, 无需修改文件属性
-2. 拷贝 BH4TDV.lua -> 路由上自己的目录下(如我自己的为/xutest), 无需修改文件属性
-3. lua /xutest/BH4TDV.lua
-
-版本Log
-V1.0: 2014-10-28, 经过深入的科学发展观而形成.
-
+2. 拷贝 lewei_dongle.lua -> 路由上自己的目录下(如我自己的为/xutest), 无需修改文件属性
+3. lua /xutest/lewei_dongle.lua
 ]]--
 
 print("--> Hello LUA, qiushui_007 test!")
+-- 设备选择, 1=有效, 0=无效
+wenshidu_valid = 1
+ds18b20_valid = 1
+sharp2_valid = 1
+
 -- load library
 local xucommon = require "luaxucommon"
 local uart = require "luauart"
@@ -94,144 +97,157 @@ function http_lewei50_2(deviceid, data, deviceid1, data1)
 end
 
 --- 读取BH4TDV之温湿度 start -------------------------------------------------------------------------
-function bh4tdv_get()
-	--device_name = "/dev/ttyATH0"
-	local device_name = "/dev/ttyUSB0"
+if wenshidu_valid == 1 then
+	function bh4tdv_get()
+		--device_name = "/dev/ttyATH0"
+		local device_name = "/dev/ttyUSB0"
 
-	local portnum = uart.open(device_name)
-	--print(portnum)
+		local portnum = uart.open(device_name)
+		--print(portnum)
 
-	-- 先赋特定值
-	f_h1 = 0.0
-	f_t1 = 0.0
+		-- 先赋特定值
+		f_h1 = 0.0
+		f_t1 = 0.0
 
-	if portnum >= 0 then
-		uart.buad_set(portnum, 0)
-		uart.flush(portnum)
-		--必须>=200
-		timeout_ms = 200
-		uart.timeout(timeout_ms)
+		if portnum >= 0 then
+			uart.buad_set(portnum, 0)
+			uart.flush(portnum)
+			timeout_ms = 200	--必须>=200
+			uart.timeout(timeout_ms)
 
-		--读取湿度和温度
-		str1 = string.char(0x01, 0x03, 0x90, 0x01, 0x00, 0x02, 0xb8, 0xcb)
-		len = string.len(str1)
-		print("tx(" .. len .. "): " .. hex(str1))
-		len_ret, str_ret = uart.tx_rx(portnum, str1, len)
-		if len_ret == 9 then
-			print("rx(" .. len_ret .. "): " .. hex(str_ret))
-			--01 03 04 01 DD 00 F9 AB B7
-			--- 判断CRC16
-			--str_tmp = string.char(0x31, 0x32, 0x33, 0x34, 0x35, 0x36)
-			crc16 = crc16_get(str_ret)
-			print("crc16 = " .. tostring(crc16) .. ", 0x" .. string.format("%04X", crc16))
+			--读取湿度和温度
+			str1 = string.char(0x01, 0x03, 0x90, 0x01, 0x00, 0x02, 0xb8, 0xcb)
+			len = string.len(str1)
+			print("tx(" .. len .. "): " .. hex(str1))
+			len_ret, str_ret = uart.tx_rx(portnum, str1, len)
+			uart.close(portnum)	--最后一定要close
 
-			f_h1 = 0.0
-			f_t1 = 0.0
-			if crc16 == 0x00 then
-				h1_h = string.byte(str_ret, 4)
-				h1_l = string.byte(str_ret, 5)
-				f_h1 = (h1_h * 256 + h1_l) / 10
+			if len_ret == 9 then
+				print("rx(" .. len_ret .. "): " .. hex(str_ret))
+				--01 03 04 01 DD 00 F9 AB B7
+				--- 判断CRC16
+				--str_tmp = string.char(0x31, 0x32, 0x33, 0x34, 0x35, 0x36)
+				crc16 = crc16_get(str_ret)
+				print("crc16 = " .. tostring(crc16) .. ", 0x" .. string.format("%04X", crc16))
 
-				t1_h = string.byte(str_ret, 6)
-				t1_l = string.byte(str_ret, 7)
-				f_t1 = (t1_h * 256 + t1_l) / 10
+				if crc16 == 0x00 then
+					h1_h = string.byte(str_ret, 4)
+					h1_l = string.byte(str_ret, 5)
+					f_h1 = (h1_h * 256 + h1_l) / 10
+
+					t1_h = string.byte(str_ret, 6)
+					t1_l = string.byte(str_ret, 7)
+					f_t1 = (t1_h * 256 + t1_l) / 10
+					print("valid: h1 = " .. tostring(f_h1) .. ", t1 = " .. tostring(f_t1) )
+				end
 			end
-			print("h1 = " .. tostring(f_h1) .. ", t1 = " .. tostring(f_t1) )
-
 		end
 
-		--最后一定要close
-		uart.close(portnum)
+		return f_h1, f_t1
 	end
 
-	return f_h1, f_t1
-end
+	f_h1, f_t1 = bh4tdv_get()
+	http_lewei50_2("T1", f_t1, "H1", f_h1)
 
-f_h1, f_t1 = bh4tdv_get()
-http_lewei50_2("T1", f_t1, "H1", f_h1)
+	--- 温度的最小和最大值
+	t_min = 10.0
+	t_max = 35.0
+	if f_t1 < t_min or f_t1 > t_max then
+	  local str_time = os.date("%Y") .. "-" .. os.date("%m") .. "-" .. os.date("%d") .. " " .. os.date("%X")
+		local content = str_time .. ", " .. str_ip .. ", " .. tostring(f_t1) .. "\nwww.lewei50.com/u/g/2375"
+		subject = "BH4DTV read error"
+		mail_tx(to_id, subject, content)
+	end
 
---- 温度的最小和最大值
-t_min = 10.0
-t_max = 35.0
-if f_t1 < t_min or f_t1 > t_max then
-  local str_time = os.date("%Y") .. "-" .. os.date("%m") .. "-" .. os.date("%d") .. " " .. os.date("%X")
-	local content = str_time .. ", " .. str_ip .. ", " .. tostring(f_t1) .. "\nwww.lewei50.com/u/g/2375"
-	subject = "BH4DTV read error"
-	mail_tx(to_id, subject, content)
 end
 --- 读取BH4TDV之温湿度 end -------------------------------------------------------------------------
 
---- 同一网关不能更新太快, 否则可能封IP
-delay_ms(1000)
+if ds18b20_valid == 1 then
+	delay_ms(1000)
 
---- 读取DS18B20的温度 start -------------------------------------------------------------------------
-function ds18b20_get()
-	local owlua = require "owlua"
+	--- 读取DS18B20的温度 start -------------------------------------------------------------------------
+	function ds18b20_get()
+		local owlua = require "owlua"
 
-	f_t2 = 0.0
-	local device_name = "USB"
-	local portnum = owlua.open(device_name)
-	if portnum > 0 then
-		f_t2 = owlua.ds18b20_get_one(portnum)
-		print("T2 = " .. tostring(f_t2))
-		owlua.close(portnum)
+		f_t2 = 0.0
+		local device_name = "USB"
+		local portnum = owlua.open(device_name)
+		if portnum > 0 then
+			f_t2 = owlua.ds18b20_get_one(portnum)
+			print("T2 = " .. tostring(f_t2))
+			owlua.close(portnum)
+		end
+
+		return f_t2
 	end
 
-	return f_t2
-end
+	f_t2 = ds18b20_get()
+	http_lewei50("T2", f_t2)
 
-f_t2 = ds18b20_get()
-http_lewei50("T2", f_t2)
+	--- 差值大于特定范围则报告
+	f_mark = 2.0
+	--f_t2 = f_t1 - f_mark - 0.1  --only test
+	f_diff = f_t1 - f_t2
+	if f_diff < -f_mark or f_diff > f_mark then
+		local str_time = os.date("%Y") .. "-" .. os.date("%m") .. "-" .. os.date("%d") .. " " .. os.date("%X")
+		local content = str_time .. ", " .. str_ip .. ", BH4TDV: " .. tostring(f_t1) .. ", 18B20: " .. tostring(f_t2)
+		print(content)
+		subject = "18b20 diff bigger than mark"
+		mail_tx(to_id, subject, content)
+	end
 
---- 差值大于特定范围则报告
-f_mark = 2.0
---f_t2 = f_t1 - f_mark - 0.1  --only test
-f_diff = f_t1 - f_t2
-if f_diff < -f_mark or f_diff > f_mark then
-	local str_time = os.date("%Y") .. "-" .. os.date("%m") .. "-" .. os.date("%d") .. " " .. os.date("%X")
-	local content = str_time .. ", " .. str_ip .. ", BH4TDV: " .. tostring(f_t1) .. ", 18B20: " .. tostring(f_t2)
-	print(content)
-	subject = "BH4DTV diff bigger than mark"
-	mail_tx(to_id, subject, content)
 end
 --- 读取DS18B20的温度 End -------------------------------------------------------------------------
 
-delay_ms(1000)
+if sharp2_valid == 1 then
+	delay_ms(1000)
 
---- read sharp2 start ------------------------------------------------------------------------
-function sharp2_get()
-	local device_name = "/dev/ttyUSB1"
+	--- read sharp2 start ------------------------------------------------------------------------
+	function sharp2_get()
+		local device_name = "/dev/ttyUSB1"
 
-	f_vo, pc_ret = uart.sharp2_get(device_name)
-	if f_vo > 0.0 then
-		if f_vo <= 3 then
-				k =  200
+		f_vo, pc_ret = uart.sharp2_get(device_name)
+		if f_vo > 0.0 then
+			if f_vo <= 3 then
+					k =  200
+			end
+			f_data = f_vo * k
+		else
+			f_data = 0.0
 		end
-		f_data = f_vo * k
-	else
-		f_data = 0.0
+
+		str_data = "PM2.5 = " .. tostring(f_data) .. ", str = " .. pc_ret .. ", vo = " .. tostring(f_vo)
+		print(str_data)
+
+		return f_data, str_data
 	end
 
-	str_data = "PM2.5 = " .. tostring(f_data) .. ", str = " .. pc_ret .. ", vo = " .. tostring(f_vo)
-	print(str_data)
+	t_min = 0.0
+	t_max = 560.0
+	invalid_times = 0
+	retry_times = 10
+	while invalid_times < retry_times do
+		f_u1, str_u1 = sharp2_get()
+		--f_u1 = 0.0	-- only test!!!
+		if f_u1 > t_min and f_u1 < t_max then
+			http_lewei50("U1", f_u1)
+			break
 
-	return f_data, str_data
+		else
+			delay_ms(500)
+			invalid_times = invalid_times + 1
+		end
+	end
+
+	if invalid_times >= retry_times then
+		local str_time = os.date("%Y") .. "-" .. os.date("%m") .. "-" .. os.date("%d") .. " " .. os.date("%X")
+		local content = str_time .. ", " .. str_ip .. ", " .. str_u1
+		print(content)
+		subject = "sharp2 reboot"
+		mail_tx(to_id, subject, content)
+
+		xucommon.reboot()	--重启, 你懂的
+	end
+
 end
-
-f_u1, str_u1 = sharp2_get()
-
-t_min = 0.0
-t_max = 560.0
---f_u1 = 0.0	-- only test!!!
-if f_u1 > t_min and f_u1 < t_max then
-	http_lewei50("U1", f_u1)
-
-else
-	local str_time = os.date("%Y") .. "-" .. os.date("%m") .. "-" .. os.date("%d") .. " " .. os.date("%X")
-	local content = str_time .. ", " .. str_ip .. ", " .. str_u1
-	print(content)
-	subject = "sharp2 data error"
-	mail_tx(to_id, subject, content)
-
-end
---- read sharp2 end ------------------------------------------------------------------------
+--- read sharp2 end ----------------------------------------------------------------------------
